bdb150de68aa08fe2381edd756d1c93050e714bb
Javier Martin Montull <javier.martin.montull@cern.ch>
2014-11-03 22:38:43 +0100

    export function updated

--- nostrikesite/views.py
+++ nostrikesite/views.py
@@ -10,24 +10,24 @@ def home(request):
 
 
 def export(request):
-	from tempfile import mkstemp
-	import os
-	import time
-	import subprocess
-	import datetime
+    from tempfile import mkstemp
+    import os
+    import time
+    import subprocess
+    import datetime
 
-	pois = PointOfInterest.objects.all()
-	serialized_queryset = serializers.serialize('json', pois)
-	fd, temp_path = mkstemp()
-	myfile = os.fdopen(fd, "w")
-	myfile.write(serialized_queryset)
-	ts = time.time()
-	st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-	myfile.write("\n\ntimestamp: " + str(st) + '\n\n')
-	myfile.close()
+    pois = PointOfInterest.objects.all()
+    serialized_queryset = serializers.serialize('json', pois)
+    fd, temp_path = mkstemp()
+    myfile = os.fdopen(fd, "w")
+    myfile.write(serialized_queryset)
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    myfile.write("\n\ntimestamp: " + str(st) + '\n\n')
+    myfile.close()
 
-	fd_return, temp_path_return = mkstemp(dir="/tmp")
-	x = subprocess.call(["/home/ubuntu/signdb.sh", temp_path, temp_path_return])
-	response = HttpResponse(open(temp_path_return).read(), content_type="text/plain")
-	response['Content-Disposition'] = 'attachment; filename=export.txt'
-	return response
+    fd_return, temp_path_return = mkstemp(dir="/tmp")
+    x = subprocess.call(["/home/ubuntu/signdb.sh", temp_path, temp_path_return])
+    response = HttpResponse(open(temp_path_return).read(), content_type="text/plain")
+    response['Content-Disposition'] = 'attachment; filename=export.txt'
+    return response
